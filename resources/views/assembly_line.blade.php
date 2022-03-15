17c583c5c70191f46436d3d4b86c747a9175c031
Janessa G. Sulit <jangeco@pricon.ph>
2022-03-15 13:51:05 +0800

    modified

--- resources/views/assembly_line.blade.php
+++ resources/views/assembly_line.blade.php
@@ -26,12 +26,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Assembly Lines</h1>
+            <h1>Assembly Liness</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-              <li class="breadcrumb-item active">Assembly Lines</li>
+              <li class="breadcrumb-item active">Assembly Liness</li>
             </ol>
           </div>
         </div>
